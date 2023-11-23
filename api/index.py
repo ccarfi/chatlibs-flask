372b025f131a0596785fef4a243fab6cf734f9ea
Christopher Carfi <ccarfi@gmail.com>
2023-11-23 13:35:57 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -21,17 +21,17 @@ def index():
 def write_story():
     user_input = request.json['topic']
     prompt = f"Write a creative, silly 75-word children's story about {user_input}. Include characters, a conflict, rising action, a surprising resolution, and a piece of short dialogue."
-//    response = openai.ChatCompletion.create(
-//        model=chatModel,
-//        messages=[
-//            {"role": "system", "content": "You are a helpful assistant."},
-//            {"role": "user", "content": prompt}
-//        ]
-//    )
-//    story_response = {
-//        "responseVariableName": "story",
-//        "value": response.choices[0].message['content']
-//    }
+#    response = openai.ChatCompletion.create(
+#        model=chatModel,
+#        messages=[
+#            {"role": "system", "content": "You are a helpful assistant."},
+#            {"role": "user", "content": prompt}
+#        ]
+#    )
+#    story_response = {
+#        "responseVariableName": "story",
+#        "value": response.choices[0].message['content']
+#    }
     story_response = {
         "responseVariableName": "echoing-the-input-back",
         "value": user_input]
